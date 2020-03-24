2a046a5521f7f3878edfc92394195154965d2472
andreyam1975 <andreyam1975@gmail.com>
2020-03-24 19:59:18 +0300

    feat: Implement method for HappyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,21 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        if (sadArray.isEmpty()) {
+            return sadArray
+        }
+
+        val happyNumbers = arrayListOf<Int>()
+        happyNumbers.add(sadArray[0])
+        for (i in 1 until sadArray.size - 1) {
+            if (sadArray[i] < (sadArray[i-1] + sadArray[i+1])) {
+                happyNumbers.add(sadArray[i])
+            }
+        }
+        happyNumbers.add(sadArray[sadArray.size - 1])
+        println("happyNumbers : $happyNumbers")
+
+        return happyNumbers.toIntArray()
     }
 }
--- src/test/kotlin/Task1HappyArrayTest.kt
+++ src/test/kotlin/Task1HappyArrayTest.kt
@@ -5,12 +5,13 @@ import subtask1.HappyArray
 class Task1HappyArrayTest {
 
     private val converter = HappyArray()
+    private val assertionMsg = "Arrays are not equal"
 
     @Test
     fun testTask1Array1() {
         val sadArray = intArrayOf(1, 5, 2, 7, 93, 8, 9, 3)
         val happyArray = intArrayOf(1, 2, 7, 8, 9, 3)
-        assertArrayEquals(happyArray, converter.convertToHappy(sadArray))
+        assertArrayEquals(assertionMsg, happyArray, converter.convertToHappy(sadArray))
     }
 
     @Test
@@ -24,27 +25,27 @@ class Task1HappyArrayTest {
     fun testTask1Array3() {
         val sadArray = intArrayOf(56, 32, 5, 31, 43, 55, 11, 87, 43, 62)
         val happyArray = intArrayOf(56, 32, 5, 11, 43, 62)
-        assertArrayEquals(happyArray, converter.convertToHappy(sadArray))
+        assertArrayEquals(assertionMsg, happyArray, converter.convertToHappy(sadArray))
     }
 
     @Test
     fun testTask1Array4() {
         val sadArray = intArrayOf(1, 2, 2, 9, 93, 2, 6, 8, 6, 12)
         val happyArray = intArrayOf(1, 2, 2, 2, 6, 8, 6, 12)
-        assertArrayEquals(happyArray, converter.convertToHappy(sadArray))
+        assertArrayEquals(assertionMsg, happyArray, converter.convertToHappy(sadArray))
     }
 
     @Test
     fun testTask1Array5() {
         val sadArray = intArrayOf(1, 3, 1)
         val happyArray = intArrayOf(1, 1)
-        assertArrayEquals(happyArray, converter.convertToHappy(sadArray))
+        assertArrayEquals(assertionMsg, happyArray, converter.convertToHappy(sadArray))
     }
 
     @Test
     fun testTask1Array6() {
         val sadArray = intArrayOf()
         val happyArray = intArrayOf()
-        assertArrayEquals(happyArray, converter.convertToHappy(sadArray))
+        assertArrayEquals(assertionMsg, happyArray, converter.convertToHappy(sadArray))
     }
 }
