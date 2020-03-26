ac1c83cb6090a7245bed6c1262b05da44844e435
andreyam1975 <andreyam1975@gmail.com>
2020-03-26 19:21:34 +0300

    Update HappyArray to pass all tests

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,23 +2,52 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
 
         if (sadArray.isEmpty()) {
             return sadArray
         }
 
-        val happyNumbers = arrayListOf<Int>()
-        happyNumbers.add(sadArray[0])
-        for (i in 1 until sadArray.size - 1) {
-            if (sadArray[i] < (sadArray[i-1] + sadArray[i+1])) {
-                happyNumbers.add(sadArray[i])
+        var intArray = sadArray.copyOf()
+
+        printArray("sadArray", sadArray)
+
+        while (!isArrayHappy(intArray)) {
+
+            val happyNumbers = arrayListOf<Int>()
+            happyNumbers.add(intArray[0])
+            for (i in 1 until intArray.size - 1) {
+                if (intArray[i] < (intArray[i-1] + intArray[i+1])) {
+                    happyNumbers.add(intArray[i])
+                }
+            }
+            happyNumbers.add(intArray[intArray.size - 1])
+
+            intArray = happyNumbers.toIntArray()
+        }
+
+        printArray("happyArray", intArray)
+
+        return intArray
+    }
+
+    private fun isArrayHappy(array: IntArray) : Boolean {
+
+        for (i in 1 until array.size - 1) {
+            if (array[i] > (array[i-1] + array[i+1])) {
+                return false
             }
         }
-        happyNumbers.add(sadArray[sadArray.size - 1])
-        println("happyNumbers : $happyNumbers")
 
-        return happyNumbers.toIntArray()
+        return true
+    }
+
+    private fun printArray(arrayName: String, array: IntArray) {
+        print("$arrayName : [")
+        for (i in 0 until array.size - 1) {
+            print("${array[i]}, ")
+        }
+        print("${array[array.size - 1]}]")
+        println()
     }
 }
