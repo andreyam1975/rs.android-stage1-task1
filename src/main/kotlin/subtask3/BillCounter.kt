5a587a21dd5f9f1c522905184fb2d507c65bb072
andreyam1975 <andreyam1975@gmail.com>
2020-03-27 09:47:09 +0300

    Create implementation for subtask3 - BillCounter

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -2,9 +2,27 @@ package subtask3
 
 class BillCounter {
 
-    // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
+    private val fairlySplit = "bon appetit"
+
+    /**
+     * The output could be "bon appetit" when bill is fair or the string with number(e.g "10"),
+     * which equal the difference between the amount [b] and half of fair bill
+     */
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+
+        var fairBill = 0
+
+        for (i in bill.indices) {
+            if (i != k) {
+                fairBill += bill[i]
+            }
+        }
+
+        return if (fairBill / 2 == b) {
+            fairlySplit
+        } else {
+            "${b - fairBill / 2}"
+        }
+
     }
 }
